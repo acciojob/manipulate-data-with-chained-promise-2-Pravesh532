8d594a5cbefdd5f6a818431a811cf8afbcb7efca
Pravesh532 <praveshpatel9569@gmail.com>
2025-04-02 16:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+ function initialPromise(){
+	  return new Promise(resolve =>{
+		  setTimeOut(()=>{
+				 resolve([1,2,3,4]);
+		  
+		  },3000)
+	  })
+ }
+
+
+initialPromise().
+	.then(array =>{
+		return  new Promise((resolve)=>{
+			setTimeOut(()=>{
+				const evenNumber = array.filter(num => num % 2 === 0);
+				document.getElementById('output').innerText = JSON.stringify(evenNumbers);
+				resolve(evenNumber);
+			},1000)
+		})
+	})
+.then(even => {
+	return new Promise((resolve)=>{
+		setTimeOut(()=>{
+			condt multi = even.map(num => num * 2);
+			document.getElementById('output').innerText = JSON.stringify(even);
+			resolve();
+		})
+	})
+})
+	
+
+ 
+
+
